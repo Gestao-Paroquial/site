54c1d2317be0ec6b19123cb99b89bea5930b95b0
Iago Mileo <mileo.iago@gmail.com>
2018-02-12 15:03:55 -0200

    Ajuste no carousel

--- Pages/Home/webroot/js/carousel.js
+++ Pages/Home/webroot/js/carousel.js
@@ -4,14 +4,20 @@ $(document).ready(() => {
             let carousel_inner = document.querySelector('.carousel-inner');
 
             res.forEach((item) => {
-                $(carousel_inner).append(
-                    `<div class="carousel-item">
-                        <img class="d-block carousel__img" src="${item.imagem}">
-                    </div>`
-                );
+                let carouse_item = carouselItemView(item);
+                $(carousel_inner).append(carouse_item);
             });
 
             carousel_inner.firstChild.nextSibling.className = "carousel-item active";
         }
     });
 });
+
+function carouselItemView(item) {
+    return `
+    <div class="carousel-item">
+        <a href="${item.destinoDoSlider}">
+            <img class="d-block carousel__img" src="${item.imagem}" alt="${item.descricao}">
+        </a>
+    </div>`
+}

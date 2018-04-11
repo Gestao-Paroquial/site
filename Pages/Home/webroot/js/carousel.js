791d5b3292fa16b75026ff8646d6a3cbe3d61328
Iago Mileo <mileo.iago@gmail.com>
2018-04-10 21:52:27 -0300

    Init casamanto

--- Pages/Home/webroot/js/carousel.js
+++ Pages/Home/webroot/js/carousel.js
@@ -1,25 +1,38 @@
-$(document).ready(() => {
-    $.get("http://localhost:8000/api/eventosHome", {}, (carousel) => {
-
-        if (carousel) {
-            let carousel_inner = document.querySelector('.carousel-inner');
-
-            carousel.forEach((item) => {
-                let carouse_item = carouselItemView(item);
-                $(carousel_inner).append(carouse_item);
-            });
-
+function buildCarousel(carousel_inner) {
+    if (carousel_inner) {
+        
+        $.get("http://localhost:8000/api/eventosHome", {}, (carousel) => {
+            
+            if (carousel) {
+                carousel.forEach((item) => {
+                    let carouse_item = carouselItemView(item);
+                    $(carousel_inner).append(carouse_item);
+                });         
+            } 
+        }).fail(function() {
+            $(carousel_inner).append(carouselItemView({
+                error: true,
+                destino: '/index.html',
+                descricao: "igreja são lucas"
+            }));
+        }).always(function () {
             carousel_inner.firstChild.nextSibling.className = "carousel-item active";
-        }
-    });
-});
+        });
+    } else {
+        setTimeout(function() {
+            buildCarousel(document.querySelector('.carousel-inner'));
+        }, 0);
+    }
+}
+buildCarousel();
 
 function carouselItemView(item) {
-    const imagem = `http://localhost:8000${item.imagem}`;
+    const imagem = !item.error ? `http://localhost:8000${item.imagem}` : 'https://www.altoastral.com.br/wp-content/uploads/2016/11/igreja-catolica.jpg';
+    
     return `
-    <div class="carousel-item">
-        <a href="${item.destino}" target="_blank">
-            <img class="d-block carousel__img" src="${imagem}" alt="${item.descricao}">
-        </a>
-    </div>`
+        <div class="carousel-item">
+            <a href="${item.destino}" target="_blank">
+                <img class="d-block carousel__img" src="${imagem}" alt="${item.descricao}">
+            </a>
+        </div>`
 }

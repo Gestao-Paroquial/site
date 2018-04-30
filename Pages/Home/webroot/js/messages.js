1def70b06b645a55f8276f940842d9a5f039cf87
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-30 17:44:54 -0300

    changing production url

--- Pages/Home/webroot/js/messages.js
+++ Pages/Home/webroot/js/messages.js
@@ -3,7 +3,7 @@ class MessagesComponent extends HTMLElement {
         super();
 
         const xmlhttp = new XMLHttpRequest();
-        const url = "http://paroquiasle.org.br/api/mensagensParoco";
+        const url = "http://laravel.paroquiasle.org.br/api/mensagensParoco";
 
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {

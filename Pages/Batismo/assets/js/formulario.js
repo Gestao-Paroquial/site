bac511343bef0808681459791e2024a7c279ad1c
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-05-24 17:00:48 -0300

    adicionando mascaras de telefone e cpf

--- Pages/Batismo/assets/js/formulario.js
+++ Pages/Batismo/assets/js/formulario.js
@@ -1,4 +1,17 @@
 $(document).ready(() => {
+  $(window).bind("load", function () {
+    $('#cpf').mask('000.000.000-00', { reverse: true });
+    $('#phone').mask('(00) 0000-00009');
+
+    $('#phone').blur(function (event) {
+      if ($(this).val().length == 15) { // Celular com 9 dígitos + 2 dígitos DDD e 4 da máscara
+        $('#phone').mask('(00) 00000-0009');
+      } else {
+        $('#phone').mask('(00) 0000-00009');
+      }
+    });
+    
+  });
   $(document).on("submit", "#batismo-form", function (event) {
     event.preventDefault();
 

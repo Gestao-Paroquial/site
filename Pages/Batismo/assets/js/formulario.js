f9dca92c427ef7d712cd5588d010cbceecd61e3f
Iago Miléo <mileo.iago@gmail.com>
2018-05-25 09:52:02 -0300

    Validação dos campos de casamento

--- Pages/Batismo/assets/js/formulario.js
+++ Pages/Batismo/assets/js/formulario.js
@@ -1,17 +1,4 @@
 $(document).ready(() => {
-  $(window).bind("load", function () {
-    $('#cpf').mask('000.000.000-00', { reverse: true });
-    $('#phone').mask('(00) 0000-00009');
-
-    $('#phone').blur(function (event) {
-      if ($(this).val().length == 15) { // Celular com 9 dígitos + 2 dígitos DDD e 4 da máscara
-        $('#phone').mask('(00) 00000-0009');
-      } else {
-        $('#phone').mask('(00) 0000-00009');
-      }
-    });
-    
-  });
   $(document).on("submit", "#batismo-form", function (event) {
     event.preventDefault();
 
@@ -26,7 +13,7 @@ $(document).ready(() => {
 
     $.ajax({
       type: "post",
-      url: `${backEndUrl}/api/pedidos`,
+      url: `http://laravel.paroquiasle.org.br/api/pedidos`,
       data: JSON.stringify(data),
       contentType: "application/json",
       traditional: true,
--- Pages/Casamento/assets/js/formulario.js
+++ Pages/Casamento/assets/js/formulario.js
@@ -1,89 +1,95 @@
 function sendMail(form) {
-    if (form) {
-        $('.casamento__form').submit(function(event) {
-            event.preventDefault();
+	if (form) {
+		$('.casamento__form').submit(function (event) {
+			event.preventDefault();
 
-            let date = $('#casamento__datepicker').val();
-            $('#casamento__datepicker').val(date.split('/').reverse().join('-'));
-            const data = formValuesToObject(form);
+			let date = $('#casamento__datepicker').val();
+			$('#casamento__datepicker').val(date.split('/').reverse().join('-'));
+			const data = formValuesToObject(form);
 
-            $.ajax({
-                type: 'post',
-                url: 'http://localhost:8000/api/registrarPedidoDeCasamento',
-                data: JSON.stringify(data),
-                contentType: "application/json; charset=utf-8",
-                traditional: true,
-                success: function(res) {
-                    if (res.success) {
-                        alert('sucesso')
-                    }
-                    if (res.error) showErrors(res.error);
-                }
-            });
-        });
-    } else {
-        setTimeout(function() {
-            sendMail(document.querySelector('.casamento__form'));
-        }, 0);
-    }
+			$.ajax({
+				type: 'post',
+				url: `http://laravel.paroquiasle.org.br/api/pedidos`,
+				data: JSON.stringify(data),
+				contentType: "application/json; charset=utf-8",
+				traditional: true,
+				success: function (res) {
+					if (res.success) sucesso(data);
+					if (res.error) showWeddingErrors(res);
+				}
+			});
+		});
+	} else {
+		setTimeout(function () {
+			sendMail(document.querySelector('.casamento__form'));
+		}, 0);
+	}
+}
+
+function showWeddingErrors(res) {
+	$('.casamento__message-error').empty();
+	for (let campo in res.error) {
+		$('.casamento__message-error').append(
+			`<p class="help-block text-danger">*${res.error[campo]}</p>`
+		);
+	}
 }
 
 sendMail(document.querySelector('.casamento__form'));
 
-window.onload = function() {
+window.onload = function () {
 
-    loadAgendaOnPicker();
-    loadParoquiaComboBox();
-
-    //ToDO: Mudar rotas do ajax
+	loadAgendaOnPicker();
+	loadParoquiaComboBox();
+	//ToDO: Mudar rotas do ajax
 }
 
 function loadParoquiaComboBox() {
-    $.ajax({
-        type: 'get',
-        url: 'http://paroquiasle.org.br/api/pastorais',
-        contentType: "application/json; charset=utf-8",
-        success: (pastorais) => {
-            pastorais.forEach((pastoral) => {
-                $('#casamento__pastoral').append($('<option>', {
-                    value: pastoral.id,
-                    text: pastoral.comunidade.nome
-                }));
-            });
-        }
-    });
+	$.ajax({
+		type: 'get',
+		url: 'http://laravel.paroquiasle.org.br/api/comunidades',
+		contentType: "application/json; charset=utf-8",
+		success: (pastorais) => {
+			pastorais.forEach((comunidade) => {
+				$('#casamento__comunidade').append($('<option>', {
+					value: comunidade.id,
+					text: comunidade.nome
+				}));
+			});
+		}
+	});
 }
 
 function loadAgendaOnPicker() {
-    var date_to_disable;
-    $.ajax({
-        type: 'get',
-        // url: `${backEndUrl}/api/agendas`,
-        url: 'http://laravel.paroquiasle.org.br/api/agenda',
-        contentType: "application/json; charset=utf-8",
-        success: (eventos) => {
-            if (eventos) {
-                eventos.forEach((evento) => {
-                    date_to_disable = evento.data_inicio_evento;
-                });
-            }
-        },
-        error: (error) => {
-            console.log(error);
-        }
-    });
+	var date_to_disable;
+	$.ajax({
+		type: 'get',
+		// url: `${backEndUrl}/api/agendas`,
+		url: 'http://laravel.paroquiasle.org.br/api/agenda',
+		contentType: "application/json; charset=utf-8",
+		success: (eventos) => {
+			if (eventos) {
+				eventos.forEach((evento) => {
+					date_to_disable = evento.data_inicio_evento;
+				});
+			}
+		},
+		error: (error) => {
+			console.log(error);
+		}
+	});
 
-    $('#casamento__datepicker').datepicker({
-        showOtherMonths: true,
-        selectOtherMonths: true,
-        dateFormat: "dd/mm/yy",
-        minDate: new Date,
-        maxDate: "31/12/2050",
-        beforeShowDay: function(date) {
-            if (date_to_disable) {
-                var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
-                return [date_to_disable.indexOf(string) == -1]
-            }
-        }
-    });
+	$('#casamento__datepicker').datepicker({
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		dateFormat: "dd/mm/yy",
+		minDate: new Date,
+		maxDate: "31/12/2050",
+		beforeShowDay: function (date) {
+			if (date_to_disable) {
+				var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+				return [date_to_disable.indexOf(string) == -1]
+			}
+		}
+	});
 }

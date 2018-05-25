9cd505a92534ad3f3c3ed2b7fda8e100196d6544
Iago Miléo <mileo.iago@gmail.com>
2018-05-25 13:50:42 -0300

    BUG FIXED: quando não há nenhum foto

--- Pages/Home/webroot/js/carousel.js
+++ Pages/Home/webroot/js/carousel.js
@@ -1,28 +1,23 @@
 function buildCarousel(carousel_inner) {
   if (carousel_inner) {
     $.get(`${backEndUrl}/api/eventosHome`, {}, carousel => {
-      if (carousel) {
+      if (carousel && carousel.length > 0) {
         carousel.forEach(item => {
           let carouse_item = carouselItemView(item);
           $(carousel_inner).append(carouse_item);
         });
+      } else {
+        insertSubImage(carousel_inner);
       }
     })
-      .fail(function() {
-        $(carousel_inner).append(
-          carouselItemView({
-            error: true,
-            destino: "/index.html",
-            descricao: "igreja são lucas"
-          })
-        );
+      .fail(function () {
+        insertSubImage(carousel_inner);
       })
-      .always(function() {
-        carousel_inner.firstChild.nextSibling.className =
-          "carousel-item active";
+      .always(function () {
+        carousel_inner.firstChild.nextSibling.className = "carousel-item active";
       });
   } else {
-    setTimeout(function() {
+    setTimeout(function () {
       buildCarousel(document.querySelector(".carousel-inner"));
     }, 0);
   }
@@ -37,9 +32,17 @@ function carouselItemView(item) {
   return `
         <div class="carousel-item">
             <a href="${item.destino}" target="_blank">
-                <img class="d-block carousel__img" src="${imagem}" alt="${
-    item.descricao
-  }">
+                <img class="d-block carousel__img" src="${imagem}" alt="${item.descricao}">
             </a>
         </div>`;
+}
+
+function insertSubImage(carousel_inner) {
+  $(carousel_inner).append(
+    carouselItemView({
+      error: true,
+      destino: "/index.html",
+      descricao: "igreja são lucas"
+    })
+  );
 }
